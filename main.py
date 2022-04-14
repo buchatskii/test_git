bf25e3dde21742908232b62faff91b4ea8472185
buchatskiy <buch16@bk.ru>
2022-04-14 22:23:57 +0300

    Task1_dz2

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+type1 = 15 * 3
+print(type(type1))
+type2 = 15 / 3
+print(type(type2))
+type3 = 15 // 3
+print(type(type3))
+type4 = 15 ** 2
+print(type(type4))
